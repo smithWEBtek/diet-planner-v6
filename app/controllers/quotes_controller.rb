9701d1f00476a2f7f2b56add9d5f0fe5391eb1a3
smithWEBtek <brad@smithwebtek.com>
2017-02-08 13:49:26 -0500

    basic crud and views for quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,24 +1,58 @@
 class QuotesController < ApplicationController
+  before_action :set_quote, only: [:show, :edit, :destroy]
 
-  def index
-    @quotes = Quote.all
-  end
+    def new
+      @quote = Quote.new
+    end
 
-  def new
-  end
+    def index
+      @quotes = Quote.all
+    end
 
-  def show
-  end
+    def show
+    end
 
-  def edit
-  end
+    def create
+      raise params.inspect
+      @quote = Quote.create(quote_params)
+      if @quote.save
+        flash[:notice] = "Quote saved."
+        redirect_to root_path
+      else
+        render :new
+      end
+    end
 
-  def update
-  end
+    def edit
+    end
 
-  def destroy
-  end
+    def update
+      @quote = Quote.find_by_id(params[:id])
+      @quote.update(quote_params)
+      if @quote.save
+        flash[:notice] = "Quote updated."
+        redirect_to quote_path(@quote)
+      else
+        redirect_to new_meal_path(@quote)
+      end
+    end
 
-  def create
+    def destroy
+      if @quote.delete
+        flash[:notice] = "Quote deleted"
+        redirect_to root_path
+      else
+        flash[:notice] = @quote.errors.full_messages
+        redirect_to quote_path(@quote)
+      end
+    end
+
+    private
+      def set_quote
+        @quote = Quote.find_by_id(params[:id])
+      end
+
+      def quote_params
+        params.require(:quote).permit(:phrase_id, :celeb_id, :verb_id, :adj_id, :food_id, :diet_id)
+      end
   end
-end
