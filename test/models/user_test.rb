f2811068521925b77394e1b2d56f3c66733b4786
smithWEBtek <brad@smithwebtek.com>
2017-02-13 13:29:06 -0500

    attempting to get test file working again

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,32 +1,27 @@
 require 'test_helper'
-
 class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
-   test 'meals_attributes=' do
+  test 'meals_attributes=' do
     params = {
       :user => {
-        :username => "charlie brown",
-        :email => "charlie@brown.com",
+        :username => "ted",
+        :email => "ted@abc.com",
         :password => "pointer",
         :weight => "190",
         :diet_id => "3",
         :meals_attributes => [
-          {:name => "breakfast", :food_id => "2", :qty => "3", :note => "I had a fine meal"},
-          {:name => "lunch", :food_id => "4", :qty => "2", :note => "You gonna eat that?"},
-          {:name => "snack", :food_id => "5", :qty => "1", :note => "Where's the beef?"},
-          {:name => "dinner", :food_id => "6", :qty => "7", :note => "What only 3 shrimps?"}
+          {:meal_name_id => "1", :food_id => "2", :qty => "3", :note => "I had a fine meal"},
+          {:meal_name_id => "2", :food_id => "4", :qty => "2", :note => "You gonna eat that?"},
+          {:meal_name_id => "3", :food_id => "6", :qty => "7", :note => "What only 3 shrimps?"}
         ]
       }
     }
-  
-  user = User.new(params[:user])
-  user.save
-
-  assert_equal user.meals.size, 4
-  # assert_equal user.meals.last.food_id, 6
-  # assert_equal user.meals.first.note, "I had a fine meal"
-  # assert_equal user.meals.first.food_id, 2
-
+    user = User.new(params[:user])
+    user.save
+    assert_equal user.meals.size, 1
+    # assert_equal user.meals.last.food_id, 6
+    # assert_equal user.meals.first.note, "I had a fine meal"
+    # assert_equal user.meals.first.food_id, 2
   end
-end 
+end
