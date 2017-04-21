c329337994e39e1925b97cf32d2f24e4e9fc70b9
smithwebtek <brad@smithwebtek.com>
2017-04-21 15:36:38 -0400

    changed to postgres for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170208122844) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "adjs", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -112,8 +115,8 @@ ActiveRecord::Schema.define(version: 20170208122844) do
     t.datetime "updated_at",                           null: false
     t.string   "provider"
     t.string   "uid"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "verbs", force: :cascade do |t|
